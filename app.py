630d2f5a3bf95ad9c2cf940dcf56b0a96ff8fde8
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2025-11-13 19:39:53 -0300

    Refactor app.py for cleaner code and version update
    Updated app version in headers and removed extra line breaks.

--- app.py
+++ app.py
@@ -13,15 +13,16 @@ payload = {"task_id":5153,"reward":5000}
 headers = {
     #'User-Agent': "Dart/3.7 (dart:io)",
     'Content-Type': "application/json",
-    'os-version': "13_33",
+    'os-version': "13_33"
+    'app-version': "1.0.2"
     'lang': "pt",
     'authorization': token,
-    'app-version': "1.0.5",
     'platform': "Android"
 }
 
 response = requests.post(url, data=json.dumps(payload), headers=headers)
 
 print(response.text)
+
 
 
